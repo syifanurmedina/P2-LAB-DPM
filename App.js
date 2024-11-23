0be4d8fdb09ee1c3bf6ac6a28b0d60f182667d7e
syifanurmedina <irishollyhock0@gmail.com>
2024-11-23 23:14:03 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Teks di tengah layar */}
+      <Text style={styles.text}>Syifa Nur Medina (5B) </Text>
+
+      {/* Container untuk dua kotak */}
+      <View style={styles.boxContainer}>
+        {/* Kotak 1 */}
+        <View style={[styles.box, {backgroundColor: 'maroon'}]} />
+        {/* Kotak 2 */}
+        <View style={[styles.box, {backgroundColor: 'pink'}]} />
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F0F0F0', // Warna abu-abu terang
+  },
+  text: {
+    fontSize: 24,
+    color: 'blue',
+    fontWeight: 'bold',
+    marginBottom: 25, // Memberikan jarak antara teks dan kotak
+  },
+  boxContainer: {
+    flexDirection: 'row', // Menyusun kotak secara horizontal
+    justifyContent: 'space-between', // Memberikan jarak merata di antara kotak
+    alignItems: 'center',
+    width: '80%', // Lebar container untuk mengatur posisi kotak
+  },
+  box: {
+    width: 100,
+    height: 100,
   },
 });
+
+export default App;
